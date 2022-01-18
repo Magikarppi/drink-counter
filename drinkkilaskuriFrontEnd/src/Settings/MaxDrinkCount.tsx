0972300c28553737767b383f8af889fd44b8ddaa
Sami H <samih90@gmail.com>
2022-01-18 19:06:32 +0200

    Started implementing MaxDrinkCount section with input

--- drinkkilaskuriFrontEnd/src/Settings/MaxDrinkCount.tsx
+++ drinkkilaskuriFrontEnd/src/Settings/MaxDrinkCount.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { colors } from '../themes';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    height: 50,
+    borderColor: colors.beige,
+    borderWidth: 2,
+    margin: 5,
+  },
+});
 
 const MaxDrinkCount = () => {
+  const [drinkCount, setDrinkCount] = useState<string>();
+
   return (
-  <View style={styles}></View>
+    <View style={styles.input}>
+      <TextInput value={drinkCount} onChangeText={setDrinkCount} placeholder='4' keyboardType='number-pad' placeholderTextColor={'grey'} textAlign='center' />
+    </View>
   )
 };
 
