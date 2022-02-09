c8e987092356a2d2a8fee46a7059e031e6a33034
Sami H <samih90@gmail.com>
2022-02-09 17:27:55 +0200

    styled drinks: amount and alc% are centered, drink name centered, icons view wraps touchable for element flex-property to work

--- drinkkilaskuriFrontEnd/src/Drink.tsx
+++ drinkkilaskuriFrontEnd/src/Drink.tsx
@@ -10,24 +10,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end',
     width: '100%',
-    height: 50,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.violet,
   },
   container: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
     alignItems: 'center',
-    width: '80%',
-    height: '100%',
-    marginRight: 15,
+    width: '100%',
+    height: 50,
+    // marginRight: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.violet,
   },
   property: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    width: 60,
-    // different flexes for star n trash 3 3 3 1 1 ?
   },
   text: {
     fontSize: 15,
@@ -40,28 +37,30 @@ const styles = StyleSheet.create({
 
 const Drink = ({ drink, addToFavorites, removeDrink }: DrinkProps) => {
   return (
-    <View style={styles.cont}>
-      <View style={styles.container}>
-        <View
-          style={{ ...styles.property, flex: 3, justifyContent: 'flex-start' }}
-        >
-          <Text style={styles.text}>{drink.name}</Text>
-        </View>
-        <View style={{ ...styles.property, flex: 2 }}>
-          <Text style={styles.text}>{`${drink.alcPercent} %`}</Text>
-        </View>
-        <View style={{ ...styles.property, flex: 2 }}>
-          <Text style={styles.text}>{`${drink.amount} dl`}</Text>
-        </View>
+    <View style={styles.container}>
+      <View
+        style={{
+          ...styles.property,
+          flex: 4,
+          justifyContent: 'center',
+        }}
+      >
+        <Text style={styles.text}>{drink.name}</Text>
+      </View>
+      <View style={{ ...styles.property, flex: 2 }}>
+        <Text style={styles.text}>{`${drink.alcPercent} %`}</Text>
+      </View>
+      <View style={{ ...styles.property, flex: 2 }}>
+        <Text style={styles.text}>{`${drink.amount} dl`}</Text>
+      </View>
+      <View style={{ ...styles.property, flex: 2 }}>
         <TouchableOpacity onPress={() => addToFavorites(drink)}>
-          <View style={{ ...styles.property, flex: 2 }}>
-            <Ionicons name="star-outline" size={30} color={colors.beige} />
-          </View>
+          <Ionicons name="star-outline" size={30} color={colors.beige} />
         </TouchableOpacity>
+      </View>
+      <View style={{ ...styles.property, flex: 2 }}>
         <TouchableOpacity onPress={() => removeDrink(drink)}>
-          <View style={{ ...styles.property, flex: 2 }}>
-            <Ionicons name="trash-outline" size={30} color={colors.violet} />
-          </View>
+          <Ionicons name="trash-outline" size={30} color={colors.violet} />
         </TouchableOpacity>
       </View>
     </View>
