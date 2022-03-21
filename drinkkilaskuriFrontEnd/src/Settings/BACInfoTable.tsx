8bd9cb87288fe4dc35537db6c901d5d604c0a212
Sami H <samih90@gmail.com>
2022-03-21 18:48:16 +0200

    Add colors to BAC-info-table to indicate the level of danger and discomfort

--- drinkkilaskuriFrontEnd/src/Settings/BACInfoTable.tsx
+++ drinkkilaskuriFrontEnd/src/Settings/BACInfoTable.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+
 import { colors } from '../themes';
 
 const styles = StyleSheet.create({
@@ -30,9 +32,28 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     padding: 5,
   },
-  text: {
+  // percentText: {
+  //   fontSize: 15,
+  //   color: colors.beige,
+  // },
+  infoText: {
     fontSize: 13,
-    color: colors.beige,
+    color: '#0e0505',
+    textShadowColor: colors.violet,
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 1,
+  },
+  // lightText: {
+  //   fontSize: 13,
+  //   color: colors.beige,
+  //   textShadowColor: 'black',
+  //   textShadowOffset: { width: 1, height: 1 },
+  //   textShadowRadius: 1,
+  // },
+  titleText: {
+    color: colors.violet,
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
@@ -40,84 +61,98 @@ class BACInfoTable extends Component {
   render(): React.ReactNode {
     return (
       <View style={styles.container}>
-        <View style={{ ...styles.rowsWrapper, flex: 1 }}>
+        <View
+          style={{
+            ...styles.rowsWrapper,
+            flex: 1,
+            backgroundColor: colors.beige,
+          }}
+        >
           <View style={styles.percentCell}>
-            <Text style={styles.text}>%</Text>
+            <Text style={styles.titleText}>%</Text>
           </View>
-          <View style={{ ...styles.textCell, alignItems: 'flex-start' }}>
-            <Text style={styles.text}>Vaikutus</Text>
+          <View
+            style={{
+              ...styles.textCell,
+              alignItems: 'flex-start',
+            }}
+          >
+            <Text style={styles.titleText}>Vaikutus</Text>
           </View>
         </View>
-        <View style={styles.rowsWrapper}>
+        <View style={{ ...styles.rowsWrapper, backgroundColor: '#25df00' }}>
           <View style={styles.percentCell}>
-            <Text style={styles.text}>{`> 0.25`}</Text>
+            <Text style={styles.infoText}>{`> 0.25`}</Text>
           </View>
           <View style={styles.textCell}>
-            <Text style={styles.text}>
+            <Text style={styles.infoText}>
               Estot vähenevät, itseluottamus kasvaa, lämmön ja hyvinvoinnin
               tunne, tarkkaavuus heikentyy.
             </Text>
           </View>
         </View>
-        <View style={styles.rowsWrapper}>
+        <View style={{ ...styles.rowsWrapper, backgroundColor: '#408500' }}>
           <View style={styles.percentCell}>
-            <Text style={styles.text}>{`0.25 \n  – \n 0.5`}</Text>
+            <Text style={styles.infoText}>{`0.25 \n  – \n 0.5`}</Text>
           </View>
           <View style={styles.textCell}>
-            <Text style={styles.text}>
+            <Text style={styles.infoText}>
               Mielihyvän tunne, kömpelyyttä, arvostelukyky heikkenee.
             </Text>
           </View>
         </View>
-        <View style={styles.rowsWrapper}>
+        <View style={{ ...styles.rowsWrapper, backgroundColor: 'yellow' }}>
           <View style={styles.percentCell}>
-            <Text style={styles.text}>{`0.5 \n – \n1.0`}</Text>
+            <Text style={styles.infoText}>{`0.5 \n – \n1.0`}</Text>
           </View>
           <View style={styles.textCell}>
-            <Text style={styles.text}>
+            <Text style={styles.infoText}>
               Reaktioaika, ajokyky ja liikkeiden hallinta heikkenevät, tunteet
               ailahtelevat.
             </Text>
           </View>
         </View>
-        <View style={styles.rowsWrapper}>
+        <View style={{ ...styles.rowsWrapper, backgroundColor: '#dd7f03' }}>
           <View style={styles.percentCell}>
-            <Text style={styles.text}>{`1.0 \n  – \n2.5`}</Text>
+            <Text style={styles.infoText}>{`1.0 \n  – \n2.5`}</Text>
           </View>
           <View style={styles.textCell}>
-            <Text style={styles.text}>
+            <Text style={styles.infoText}>
               Heikkeneminen voimistuu, pahoinvointia, oksennuksia, sekavuutta.
             </Text>
           </View>
         </View>
-        <View style={styles.rowsWrapper}>
+        <View style={{ ...styles.rowsWrapper, backgroundColor: colors.danger }}>
           <View style={styles.percentCell}>
-            <Text style={styles.text}>{`2.5 \n  – \n4.0`}</Text>
+            <Text style={styles.infoText}>{`2.5 \n  – \n4.0`}</Text>
           </View>
           <View style={styles.textCell}>
-            <Text style={styles.text}>
+            <Text style={styles.infoText}>
               Puhe sammaltaa, näköhäiriöitä, tajuttomuus.
             </Text>
           </View>
         </View>
-        <View style={styles.rowsWrapper}>
+        <View style={{ ...styles.rowsWrapper, backgroundColor: '#a10000' }}>
           <View style={styles.percentCell}>
-            <Text style={styles.text}>{`> 0.4`}</Text>
+            <Text style={styles.infoText}>{`> 4.0`}</Text>
           </View>
           <View style={styles.textCell}>
-            <Text style={styles.text}>
+            <Text style={styles.infoText}>
               Hengitys vaikeutuu, verensokeri vähenee, lämmöntuotanto heikkenee.
             </Text>
           </View>
         </View>
-        <View style={styles.rowsWrapper}>
+        <View style={{ ...styles.rowsWrapper, backgroundColor: '#0e0505' }}>
           <View style={styles.percentCell}>
-            <Text style={styles.text}>{`> 0.5`}</Text>
+            <Text style={styles.infoText}>{`> 5.0`}</Text>
           </View>
-          <View style={styles.textCell}>
-            <Text style={styles.text}>
-              Keskimäärin tappava pitoisuus (pääkallo icon?)
-            </Text>
+          <View
+            style={{
+              ...styles.textCell,
+              alignItems: 'center',
+            }}
+          >
+            <Ionicon name="skull" color={colors.backgroundDark} size={35} />
           </View>
         </View>
       </View>
