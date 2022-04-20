95fb56cec3de8ad6f3949da7f39035e1f9a8edaa
Sami H <samih90@gmail.com>
2022-04-20 16:06:08 +0300

    Change showing sober-text to state, rename text state, fix getting of hours and minutes, add better check when to show sober-text and not

--- drinkkilaskuriFrontEnd/src/StatusMoreInfo.tsx
+++ drinkkilaskuriFrontEnd/src/StatusMoreInfo.tsx
@@ -22,40 +22,56 @@ const styles = StyleSheet.create({
   },
 });
 
-const youAreSober = 'Olet kuluttanut kaiken alkoholin verestäsi';
+const soberText = 'Olet kuluttanut kaiken alkoholin verestäsi';
 
 const StatusMoreInfo = ({ totalBac }: StatusMoreInfoProps) => {
-  const [soberingText, setSoberingText] = useState<string>(youAreSober);
+  const [showSoberText, setShowSoberText] = useState<boolean>(false);
+  const [textToShow, setTextToShow] = useState<string>(soberText);
 
   useEffect(() => {
-    const whenSober = calculateWhenSober(totalBac).toFixed(2);
+    const whenSober = calculateWhenSober(totalBac).toFixed(5);
 
     // Turn into 60min format
     const indexOfDot = whenSober.indexOf('.');
     const whenSoberHours = parseInt(whenSober.slice(0, indexOfDot), 10);
-    const whenSoberMinutes = Math.round(
-      parseInt(whenSober.slice(indexOfDot + 1), 10) * 0.6
+    const minsRaw = parseFloat(whenSober.slice(indexOfDot));
+    const whenSoberMinutesDec = (minsRaw * 60).toString();
+    const indexOfMinsDot = whenSoberMinutesDec.indexOf('.');
+    const whenSoberMinutes = parseInt(
+      whenSoberMinutesDec.slice(0, indexOfMinsDot),
+      10
     );
+
     const notSoberText = ` ${
       whenSoberHours > 0 ? whenSoberHours + ' t' : ''
     } ${whenSoberMinutes} min \n `;
 
-    if (parseInt(whenSober, 10) <= 0) {
+    const whenSoberFloat = parseFloat(whenSober);
+    if (
+      whenSoberFloat <= 0 ||
+      (whenSoberHours === 0 && whenSoberMinutes === 0)
+    ) {
       // if sober set the sober text
-      setSoberingText(youAreSober);
+      setShowSoberText(true);
+      setTextToShow(soberText);
     } else {
       // if not-sober set the not-sober text
-      setSoberingText(notSoberText);
+      setShowSoberText(false);
+      setTextToShow(notSoberText);
     }
   }, [totalBac]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.soberText}>
-        Olet taas selvänä {'\n'} n.
-        <Text style={styles.soberHourText}>{soberingText}</Text>
-        kuluttua.
-      </Text>
+      {showSoberText ? (
+        <Text style={styles.soberText}>{textToShow}</Text>
+      ) : (
+        <Text style={styles.soberText}>
+          Olet taas selvänä {'\n'} n.
+          <Text style={styles.soberHourText}>{textToShow}</Text>
+          kuluttua.
+        </Text>
+      )}
     </View>
   );
 };
